5169ff9e0dc36bea11796b2d861d0de94d705953
Piyush Chauhan <piyushgchauhan01@gmail.com>
2021-12-25 17:08:40 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,68 @@
+
+import React,{useState,useEffect} from 'react';
+
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Axios from 'axios'
+import {Button} from 'native-base'
+import User from './components/User';
+
+const App = () => {
+  const [details,setDetails] = useState(null)
+
+  const fetchDetails = async () =>{
+    try{
+      const {data} = await Axios.get('https://randomuser.me/api/') 
+      const details = data.results[0]
+      console.log(details)
+      setDetails(details)
+    } catch(error){
+      console.log(error)
+    }
+  } 
+
+  useEffect(() => {
+    fetchDetails()
+  },[])
+  
+  if (!details) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    )
+  } else{
+    return(
+      <View style={styles.container}>
+        <View>
+          <User details={details}/>
+          <Button rounded style={styles.button} onPress={() => fetchDetails()}>
+            <Text>New User</Text>
+          </Button>
+        </View>
+      </View>
+    )
+
+  }
+
+}
+export default App;
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#222831',
+  },
+  button:{
+    marginTop:30,
+    paddingHorizontal:30,
+  }
+})
